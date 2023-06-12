039d4564669070ccf0457ab25f5b3df4c4d7dbae
JuanmaEiroa <eiroajm@gmail.com>
2023-06-12 18:38:57 -0300

    Preentrega 2: Inicio de paginación y views

--- src/routers/views.router.js
+++ src/routers/views.router.js
@@ -4,9 +4,19 @@ import messageManager from "../dao/dbmanagers/message.manager.js";
 
 const viewsRouter = Router();
 
-viewsRouter.get("/", async (req, res) => {
-  const renderProdList = await productManager.getProducts();
-  res.render("home", { renderProdList });
+viewsRouter.get("/products", async (req, res) => {
+  let page = parseInt(req.query.page);
+  if (!page) page = 1;
+  let result = await productManager.getProducts();
+  console.log(result)
+  result.prevLink = result.hasPrevPage
+    ? `http://localhost:8080/products?page=${result.prevPage}`
+    : "";
+  result.nextLink = result.hasNextPage
+    ? `http://localhost:8080/products?page=${result.nextPage}`
+    : "";
+  result.isValid = !(page <= 0 || page > result.totalPages);
+  res.render("products",  result );
 });
 
 viewsRouter.get("/realtimeproducts", async (req, res) => {
@@ -14,9 +24,9 @@ viewsRouter.get("/realtimeproducts", async (req, res) => {
   res.render("realTimeProducts", { renderProdList });
 });
 
-viewsRouter.get("/chat", async(req,res)=> {
+viewsRouter.get("/chat", async (req, res) => {
   const renderMessages = await messageManager.getMessages();
-  res.render("chat", {renderMessages})
-})
+  res.render("chat", { renderMessages });
+});
 
 export default viewsRouter;
