8141efecf7434fc9a1c20788d9714b8fa2f2e8da
JuanmaEiroa <eiroajm@gmail.com>
2023-10-10 03:32:59 -0300

    Entrega Final - Mejoras del Proyecto - Envío de mail con eliminación de producto en usuario Premium

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,5 +1,7 @@
 //Importaciones
 import { productService } from "../repositories/repoIndex.js";
+import { mailerTransport } from "../utils/mailer.js";
+import { appConfig } from "../config/env.config.js";
 
 //Creación del controlador de productos
 class ProductController {
@@ -29,6 +31,25 @@ class ProductController {
 
   //Borrar un producto por su ID
   async delete(pid) {
+    //Se obtiene el dueño del producto. Si es un usuario premium, se genera y envía un mail a su correo avisandole la eliminación.
+    const prodToDelete = await this.service.getById(pid);
+    if (prodToDelete.owner !== "Admin") {
+      let mail = await mailerTransport.sendMail({
+        from: `CoderCommerce ${appConfig.gmailUser}`,
+        to: prodToDelete.owner,
+        subject: "Eliminación de su Producto",
+        html: `
+                      <div>
+                      <h1>Eliminación de su producto de nuestra tienda</h1>
+                      <p>Le informamos que su producto ha sido removido de nuestro catálogo</p>
+                      <p>Puede ver el nuevo catálogo ingresando en el siguiente enlace:</p>
+                      <a href="http://localhost:8080/"><button>Ir al catálogo</button></a>
+                      </div>
+                `,
+        attachments: [],
+      });
+    }
+    //Se elimina el producto
     return await this.service.delete(pid);
   }
 }
